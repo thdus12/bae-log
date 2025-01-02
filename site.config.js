29626efadf351aa9b345d9293244c480c9f8bb43
brie <brie@spadecompany.kr>
2025-01-03 08:56:40 +0900

    feat. 새 템플릿 적용

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "소연",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Backend developer",
     bio: "java girl이 되기 위한 여정.",
     email: "bthdus5890@gmail.com",
@@ -25,8 +25,8 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  since: 2024, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
@@ -69,7 +69,7 @@ const CONFIG = {
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+  revalidateTime: 10 * 6, // revalidate time for [slug], index
 }
 
 module.exports = { CONFIG }
--- src/libs/utils/notion/types.ts
+++
@@ -1,9 +0,0 @@
-import { PropertyType } from "notion-types"
-
-export type ExtendedPropertyType = PropertyType | 'code'
-
-export interface CodeBlockContent {
-  code: string
-  language: string
-  colorFormat: string
-}
