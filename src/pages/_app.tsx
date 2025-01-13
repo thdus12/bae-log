64552b819c9e056ccbc004ebf59cdaab18d37a3d
배소연 <97299700+thdus12@users.noreply.github.com>
2025-01-13 15:08:25 +0900

    feat: pdf 파일 다운로드 기능 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,28 @@
+// pages/_app.tsx
 import { AppPropsWithLayout } from "../types"
 import { Hydrate, QueryClientProvider } from "@tanstack/react-query"
 import Sparkle from "src/components/Sparkle"
 import { RootLayout } from "src/layouts"
 import { queryClient } from "src/libs/react-query"
+import { useRouter } from 'next/router'
 
 import "/src/styles/themes/prism.css"
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter()
+  const isPrintPage = router.pathname.startsWith('/print')
   const getLayout = Component.getLayout || ((page) => page)
 
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
+        {isPrintPage ? (
+          getLayout(<Component {...pageProps} />)
+        ) : (
+          <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
+        )}
       </Hydrate>
-      {/* <AnimatedCursor /> */}
-      <Sparkle />
+      {!isPrintPage && <Sparkle />}
     </QueryClientProvider>
   )
 }
