db7c3e86f5ad9013a6baae6cb66a885abc3700e6
배소연 <97299700+thdus12@users.noreply.github.com>
2025-01-13 15:40:19 +0900

    feat: pdf 파일 다운로드 기능 추가

--- src/pages/print/[slug].tsx
+++ src/pages/print/[slug].tsx
@@ -32,7 +32,7 @@ export const getStaticProps: GetStaticProps = async (context) => {
   const slug = context.params?.slug
 
   const posts = await getPosts()
-  const detailPosts = filterPosts(posts, filter)  // 동일한 filter 적용
+  const detailPosts = filterPosts(posts, filter)
   const postDetail = detailPosts.find((t: any) => t.slug === slug)
   const recordMap = await getRecordMap(postDetail?.id!)
 
@@ -82,18 +82,36 @@ const PrintPage = () => {
 export default PrintPage
 
 const PrintWrapper = styled.div`
-  background: white;
-  width: 100%;  // 최대 너비 제한 제거
+  padding: 3rem 1.5rem;
+  border-radius: 1.5rem;
+  max-width: 70rem; // 56rem에서 증가
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+  0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  margin: 0 auto;
 
   .print-content {
     margin: 0 auto;
-    width: 100%;  // 최대 너비 제한 제거
+    max-width: 60rem; // 42rem에서 증가
+    width: 100%;
   }
 
   @media print {
     @page {
       size: A4;
       margin: 20mm;
+    }
+    
+    padding: 0;
+    max-width: none;
+    
+    .print-content {
+      max-width: none;
+    }
+
+    html, body {
+      zoom: 62%;
+      -webkit-print-color-adjust: exact;
+      print-color-adjust: exact;
     }
   }
 `
